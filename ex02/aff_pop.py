315687621c240be50b273ee245d4ccdc07c4ccf9
bramariene <bramariene@laposte.net>
2025-03-06 19:18:47 -0500

    ex02 done

--- ex02/aff_pop.py
+++ ex02/aff_pop.py
@@ -2,6 +2,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from load_csv import load
 
+def convert_to_string(value):
+    if value >= 1_000_000_000:
+        return f"{int(value / 1_000_000_000)}B"
+    elif value >= 1_000_000:
+        return f"{int(value / 1_000_000)}M"
+    elif value >= 1_000:
+        return f"{int(value / 1_000)}k"
+    else:
+        return f"{int(value)}"
+
 def convert_to_int(value):
     if 'B' in value:
         return int(float(value.replace('B', '')) * 1_000_000_000)
@@ -18,13 +28,17 @@ def aff_pop(df: pd.DataFrame, country1: str, country2: str):
     row_c2 = df.loc[country2, (df.columns >= '1800') & (df.columns <= '2050')]
     c1_conv = row_c1.apply(convert_to_int)
     c2_conv = row_c2.apply(convert_to_int)
-    plt.plot(row_c1.index, c1_conv.values, label=country1)
-    plt.plot(row_c2.index, c2_conv.values, label=country2)
+    plt.plot(row_c1.index, c1_conv.values, label=country1, color="green")
+    plt.plot(row_c2.index, c2_conv.values, label=country2, color="blue")
     plt.xticks(row_c1.index[::40])
-
-    # print(row_c2.max())
-    # step = int(max(row_c1.values.max(), row_c2.max()))
-    # plt.yticks([step, step * 2, step * 3])
+    min_v = min(c1_conv.min(), c2_conv.min())
+    max_v = max(c1_conv.max(), c2_conv.max())
+    tick_25 = min_v + 0.3 * (max_v - min_v)
+    tick_50 = min_v + 0.6 * (max_v - min_v)
+    tick_75 = min_v + 0.9 * (max_v - min_v)
+    ticks = [tick_25, tick_50, tick_75]
+    ticks_label = [convert_to_string(tick) for tick in ticks]
+    plt.yticks(ticks, ticks_label)
     plt.xlabel('Year')
     plt.ylabel('Life expectancy')
     plt.title('Population Projections')
@@ -36,7 +50,7 @@ def main():
     try:
         if df is None:
             raise ValueError("Dataframe is None")
-        aff_pop(df, "France", "China")
+        aff_pop(df, "France", "Belgium")
     except (KeyError, ValueError) as e:
             print(f"{type(e).__name__}: Invalid key for row selections")
     return 0
